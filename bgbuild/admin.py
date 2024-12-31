44a0b8668f3426e2561990d51c4bd71a00dea58b
Alexander Baker <alex.jbaker20@googlemail.com>
2024-12-31 09:57:54 +0000

    maint - add summernote admin tools

--- bgbuild/admin.py
+++ bgbuild/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Bgbuild, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Bgbuild)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('build_title', 'slug', 'status', 'created_on')
+    search_fields = ['build_title', 'content']
+    list_filter = ('status', 'created_on',)
+    prepopulated_fields = {'slug':('build_title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Bgbuild)
 admin.site.register(Review)
--- bgbuild/views.py
+++ bgbuild/views.py
@@ -4,5 +4,5 @@ from .models import Bgbuild
 
 # Create your views here.
 class BuildList(generic.ListView):
-    queryset = Bgbuild.objects.all()
+    queryset = Bgbuild.objects.all().order_by("-created_on")
     template_name = "bgbuild/build_list.html"
