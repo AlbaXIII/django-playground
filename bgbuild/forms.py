7d6969948343f3644128203deef88e679391d3e4
Alexander Baker <alex.jbaker20@googlemail.com>
2025-01-10 15:29:52 +0000

    feat - post comment as auth user

---
+++ bgbuild/forms.py
@@ -0,0 +1,7 @@
+from .models import Review
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('content',)
--- bgbuild/views.py
+++ bgbuild/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Bgbuild
+from .forms import CommentForm
+from django.contrib import messages
+
 
 # Create your views here.
 class BuildList(generic.ListView):
@@ -25,6 +28,19 @@ def build_detail(request, slug):
     build = get_object_or_404(queryset, slug=slug)
     reviews = build.reviews.all().order_by("review_date")
     reviews_count = build.reviews.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.build = build
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -33,5 +49,6 @@ def build_detail(request, slug):
             "build": build,
             "reviews": reviews,
             "reviews_count": reviews_count,
+            "comment_form": comment_form,
         },
     )
